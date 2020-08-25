93ba9025e2ee0dbff6f30141a2847bcbdbbed03d
clegaard <clegaard@outlook.com>
2020-08-25 10:17:01 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+apt update
+apt upgrade
+apt install build-essential libzmq3-dev python3 python3-distutils curl git cmake 
+
+# change to temporary directory to avoid cluttering
+cd /tmp
+
+# pip, does not seem to be included in the default python distribution
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+
+# install vscode
+curl -o code.deb -L http://go.microsoft.com/fwlink/?LinkID=760868
+dpkg -i code.deb
